e4eed5ad8345aae070b9f718655aa920ac95497e
Farhan Mohammed <farhanhm12@gmail.com>
2020-01-12 12:35:03 -0500

    Updates

---
+++ precache-manifest.9f1ed75980b8e5c311e8889f2bdb7705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da695c35e8306709cfefd2398f2efed1",
+    "url": "/EvidEvis/index.html"
+  },
+  {
+    "revision": "7aa89ed85a3b9eab3cb7",
+    "url": "/EvidEvis/static/css/main.7d752c0c.chunk.css"
+  },
+  {
+    "revision": "40f1bd8d02f023fc6f40",
+    "url": "/EvidEvis/static/js/2.ba4904f6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/EvidEvis/static/js/2.ba4904f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7aa89ed85a3b9eab3cb7",
+    "url": "/EvidEvis/static/js/main.a1324512.chunk.js"
+  },
+  {
+    "revision": "0074b1e398103a6fd896",
+    "url": "/EvidEvis/static/js/runtime-main.d3332eda.js"
+  },
+  {
+    "revision": "ca0086a0f708d3e3acdc4d768866d67d",
+    "url": "/EvidEvis/static/media/map.ca0086a0.png"
+  }
+]);
